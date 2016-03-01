5cd6d92cf392cf77aee192d6787920d75caa783f
Adrian Pena <adrian.pena@dominos.com>
2016-02-29 23:19:55 -0500

    Added a test command-line runner to demonstrate the database configuration

--- src/main/java/com/mdp/bank/MdpBankApplication.java
+++ src/main/java/com/mdp/bank/MdpBankApplication.java
@@ -1,12 +1,40 @@
 package com.mdp.bank;
 
+import com.mdp.bank.model.Customer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MdpBankApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MdpBankApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner test(final CustomerRepository customerRepository) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				Customer customer1 = new Customer();
+				customer1.setEmail("rastadrian@gmail.com");
+				customer1.setName("Adrian");
+				customer1.setLastName("Pena");
+
+				customer1 = customerRepository.save(customer1);
+				System.out.println("This is the saved customer1 id: " + customer1.getId());
+
+				Customer customer2 = new Customer();
+				customer2.setEmail("anvitha@gmail.com");
+				customer2.setName("Anvitha");
+				customer2.setLastName("Paruchuri");
+
+				customer2 = customerRepository.save(customer2);
+				System.out.println("This is the saved customer1 id: " + customer2.getId());
+
+			}
+		};
 	}
 }
